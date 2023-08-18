a368a8b38da05dd881be6573d6f2a3efed895ba3
kris <vae99cht66@163.com>
2023-08-18 16:03:01 +0800

    style: delete model test from open_client_test.go source file

--- openai_client_test.go
+++ openai_client_test.go
@@ -1,15 +1,1 @@
 package echoopenai
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestListModels(t *testing.T) {
-	client := NewClient("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")
-	models, err := client.ListModels()
-	if err != nil {
-		t.Error("test ListModes func failed")
-	}
-	fmt.Printf("%v", models)
-}
