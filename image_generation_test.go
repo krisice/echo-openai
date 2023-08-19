8659b705275d81517241d897ed4dc81648adec07
kris <vae99cht66@163.com>
2023-08-19 13:05:36 +0800

    test: rewrite image generation

--- image_generation_test.go
+++ image_generation_test.go
@@ -1,42 +1,70 @@
 package echoopenai
 
 import (
+	"bytes"
 	"encoding/base64"
+	"fmt"
+	"image/png"
 	"os"
 	"testing"
 )
 
-func TestCreateImageGeneration(t *testing.T) {
+func TestCreateURLFormatImageGeneration(t *testing.T) {
 	apiKey := os.Getenv("ECHOOPENAIAPIKEY")
 	client := NewClient(apiKey)
+
 	req := ImageGenerationRequest{
 		Prompt:         "black cat",
 		ResponseFormat: ImageFormatURL,
+		N:              1,
 	}
 	res, err := client.CreateImageGeneration(req)
 	if err != nil {
 		t.Errorf("test generate url format image failed %v", err)
+		return
 	}
-	t.Logf("test generate url format image successed")
+	t.Logf("test generate url format image successed %v", res)
+}
 
-	req = ImageGenerationRequest{
-		Prompt:         "black cat",
+func TestCreateB64JSONFormatImageGeneration(t *testing.T) {
+	apiKey := os.Getenv("ECHOOPENAIAPIKEY")
+	client := NewClient(apiKey)
+
+	req := ImageGenerationRequest{
+		Prompt:         "color cat",
 		ResponseFormat: ImageFormatBase64JSON,
+		N:              1,
 	}
 
-	res, err = client.CreateImageGeneration(req)
+	res, err := client.CreateImageGeneration(req)
 	if err != nil {
 		t.Errorf("test generate base64 json format image failed %v", err)
+		return
 	}
 
-	bytes, err := base64.StdEncoding.DecodeString(res.Data[0].Base64JSON)
+	imgBytes, err := base64.StdEncoding.DecodeString(res.Data[0].Base64JSON)
 	if err != nil {
 		t.Errorf("test generate base64 json format image failed %v", err)
+		return
 	}
 
-	err = os.WriteFile("images/image_generation.png", bytes, 0666)
+	r := bytes.NewReader(imgBytes)
+	imgData, err := png.Decode(r)
 	if err != nil {
-		t.Errorf("test generate base64 json format image failed %v", err)
+		fmt.Printf("PNG decode error: %v\n", err)
+		return
+	}
+
+	file, err := os.Create("images/image_generation.png")
+	if err != nil {
+		fmt.Printf("File creation error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, imgData); err != nil {
+		fmt.Printf("PNG encode error: %v\n", err)
+		return
 	}
 
 	t.Logf("test generate base64 json format image successed")
